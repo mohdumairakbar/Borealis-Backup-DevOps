98cdea0bec3e194cf28141d989d3864107ad136d
Sachin Sharma <sachinsharm_a@hcl.com>
2020-07-17 06:37:30 +0000

    Updated spaks.ps1

--- ARM_PS-Scripts/Service Principle/spaks.ps1
+++ ARM_PS-Scripts/Service Principle/spaks.ps1
@@ -2,7 +2,7 @@ try
 {
 	# creating service principal for aks cluster
 
-	az ad sp create-for-rbac -n "aksinfrapocsp" --skip-assignment
+	az ad sp create-for-rbac -n "SPNaksinfrapoc" --skip-assignment
 
 	# Service Principal for aks cluster created
 }
