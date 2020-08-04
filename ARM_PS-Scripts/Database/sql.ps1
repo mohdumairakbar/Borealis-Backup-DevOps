55a80aadc5df7ee48890b32048a023b64a42059a
Sachin Sharma <sachinsharm_a@hcl.com>
2020-08-04 04:08:43 +0000

    Added sql.ps1

---
+++ ARM_PS-Scripts/Database/sql.ps1
@@ -0,0 +1,31 @@
+# This script creates a SQL Database
+
+# You'll need to adjust these variables accordingly
+
+$sqlAdminUserName = 'borealisadmin'
+$sqlAdminPassword = 'borealispassword@123'
+$sqlServerName = "brseuwsqldbdpoc01" # this needs to be all lower case
+$resourceGroupName = "BRS-EUW-RSG-D-SPOKE-AKS" 
+$sqlDatabaseName = 'EMPDB01'
+$location = 'westeurope'
+
+#Write-Host "Creating resource group $resourceGroupName"
+#az group create --name $resourceGroupName --location $location
+
+Write-Host "Creating SQL Server $sqlServerName"
+az sql server create `
+    --name $sqlServerName `
+    --resource-group $resourceGroupName `
+    --location $location  `
+    --admin-user $sqlAdminUserName `
+    --admin-password $sqlAdminPassword
+
+Write-Host "Creating database $sqlDatabaseName"
+az sql db create `
+	--resource-group $resourceGroupName `
+	--server $sqlServerName `
+	--name $sqlDatabaseName `
+	--service-objective S0
+
+Write-Host "Creating firewall rule..."
+az sql server firewall-rule create -g $resourceGroupName -s $sqlServerName -n "allowAzure" --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
