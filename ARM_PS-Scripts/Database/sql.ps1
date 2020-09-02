13a07d40f8460a48f1243123386926377350e91c
Sachin Sharma <sachinsharm_a@hcl.com>
2020-09-02 13:38:10 +0000

    Updated sql.ps1

--- ARM_PS-Scripts/Database/sql.ps1
+++ ARM_PS-Scripts/Database/sql.ps1
@@ -4,9 +4,9 @@
 
 $sqlAdminUserName = 'borealisadmin'
 $sqlAdminPassword = $env:SQLDBDEV
-$sqlServerName = "brseuwsqldbdpoc08" # this needs to be all lower case
+$sqlServerName = "brseuwsqldbdpoc09" # this needs to be all lower case
 $resourceGroupName = "BRS-EUW-RSG-D-SPOKE-AKS" 
-$sqlDatabaseName = 'EMPDB09'
+$sqlDatabaseName = 'EMPDB08'
 $location = 'westeurope'
 
 #Write-Host "Creating resource group $resourceGroupName"
